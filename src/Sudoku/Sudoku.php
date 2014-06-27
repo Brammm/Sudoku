732e3ae6249fb8a8f1988b1445c705eedbc709f7
Bram Van der Sype <bram.vandersype@gmail.com>
2014-06-27 13:23:36 +0200

    Created base class with Symfony2 container

---
+++ src/Sudoku/Sudoku.php
@@ -0,0 +1,24 @@
+<?php
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class Sudoku
+{
+    /** @var ContainerInterface */
+    private $container;
+
+    public function __construct()
+    {
+        $this->container = new ContainerBuilder();
+        $loader = new YamlFileLoader($this->container, new FileLocator(__DIR__));
+        $loader->load('Resources/config/services.yml');
+    }
+
+    public function getContainer()
+    {
+        return $this->container;
+    }
+} 
